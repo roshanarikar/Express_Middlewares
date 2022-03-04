41b2cefde73813aefb076270f960c6268d561f34
Roshan Arikar <roshan.g.arikar@gmail.com>
2022-03-05 00:02:54 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/books", (req, res) => {
+    console.log ( "Fetching all books")
+});
 
 app.listen(5000, () =>{
-    console.log("listening  on port 5000")
+    console.log("Hello I'm listening  on port 5000")
 })
